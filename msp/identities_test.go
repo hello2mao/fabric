91002ddd4511b690d392547cf09c8ef5d07f74a7
Hongbin Mao <hello2mao@gmail.com>
2020-05-20 19:13:31 +0800

    add two case
    Signed-off-by: Hongbin Mao <hello2mao@gmail.com>

---
+++ msp/identities_test.go
@@ -0,0 +1,69 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package msp
+
+import (
+	"crypto/x509"
+	"testing"
+	"time"
+
+	"github.com/hyperledger/fabric-protos-go/msp"
+	"github.com/hyperledger/fabric/bccsp"
+	"github.com/hyperledger/fabric/bccsp/sw"
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	testMSPName = "testMSP"
+)
+
+func TestNewIdentity(t *testing.T) {
+	cryptoProvider, err := sw.NewDefaultSecurityLevelWithKeystore(sw.NewDummyKeyStore())
+	assert.NoError(t, err)
+	mspImpl := &bccspmsp{
+		name:         testMSPName,
+		bccsp:        cryptoProvider,
+		opts:         &x509.VerifyOptions{Roots: x509.NewCertPool(), Intermediates: x509.NewCertPool()},
+		cryptoConfig: &msp.FabricCryptoConfig{IdentityIdentifierHashFunction: bccsp.SHA256},
+	}
+
+	// generate self signed cert for test
+	now := time.Unix(100000, 0)
+	_, cert := generateSelfSignedCert(t, now)
+	mspImpl.opts.Roots.AddCert(cert)
+	certPubK, err := mspImpl.bccsp.KeyImport(cert, &bccsp.X509PublicKeyImportOpts{Temporary: true})
+	assert.NoError(t, err)
+
+	identity, err := newIdentity(cert, certPubK, mspImpl)
+	assert.NoError(t, err)
+	assert.Equal(t, testMSPName, identity.GetIdentifier().Mspid)
+	assert.Equal(t, true, len(identity.GetIdentifier().Id) != 0)
+	assert.NotNil(t, identity.Validate())
+}
+
+func TestExpiresAt(t *testing.T) {
+	cryptoProvider, err := sw.NewDefaultSecurityLevelWithKeystore(sw.NewDummyKeyStore())
+	assert.NoError(t, err)
+	mspImpl := &bccspmsp{
+		name:         testMSPName,
+		bccsp:        cryptoProvider,
+		opts:         &x509.VerifyOptions{Roots: x509.NewCertPool(), Intermediates: x509.NewCertPool()},
+		cryptoConfig: &msp.FabricCryptoConfig{IdentityIdentifierHashFunction: bccsp.SHA256},
+	}
+
+	// generate self signed cert for test
+	now := time.Unix(100000, 0)
+	_, cert := generateSelfSignedCert(t, now)
+	mspImpl.opts.Roots.AddCert(cert)
+	certPubK, err := mspImpl.bccsp.KeyImport(cert, &bccsp.X509PublicKeyImportOpts{Temporary: true})
+	assert.NoError(t, err)
+
+	identity, err := newIdentity(cert, certPubK, mspImpl)
+	assert.NoError(t, err)
+	// the self gen cert sets cert.NotAfter at one hour later
+	assert.Equal(t, now.Add(1 * time.Hour), identity.ExpiresAt().Local())
+}
